6238485e05a15f6378b4f3f2f01546fb83e4dad2
vitalspace <myglobalsounds@gmail.com>
2023-07-05 15:41:17 -0600

    BubbleSort with C

--- bubbleSort/bubbleSort.c
+++ bubbleSort/bubbleSort.c
@@ -4,7 +4,7 @@ int *bubbleSort(int arr[], int size)
 {
     for (int i = 0; i < size - 1; i++)
     {
-        for (int j = 0; j < size - i - 1; j++)
+        for (int j = 0; j < size - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
@@ -27,7 +27,6 @@ int main()
     {
         printf("%d ", sortedArray[i]);
     }
-    printf("\n");
 
     return 0;
 }
